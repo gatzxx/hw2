4e443296c700c1af3cc9f2347d8a9e2f73f19663
Egor K <murwoofmur@gmail.com>
2025-01-23 10:23:11 +0700

    hw1 is done

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -5,18 +5,22 @@ import s2 from '../../s1-main/App.module.css'
 import FriendMessage from './friend-message/FriendMessage'
 import avatar from './avatar.png'
 
-/*
-* 1 - описать тип MessageType
-* 2 - описать тип MessagePropsType в файле Message.tsx
-* 3 - в файле Message.tsx отобразить приходящие данные
-* 4 - выполнить пункты 2, 3 в файле FriendMessage.tsx
-* 5 - сделать стили в соответствии с дизайном
-* */
+export type MessageType = {
+    id: number
+    user: User
+    message: Message
+}
 
-// нужно создать правильный тип вместо any
-export type MessageType = any
+type User = {
+    avatar: string
+    name: string
+}
 
-// структуру объекта не менять
+type Message = {
+    text: string
+    time: string
+}
+
 export const message0: MessageType = {
     id: 0,
     user: {
@@ -45,13 +49,11 @@ const HW1 = () => {
         <div id={'hw1'}>
             <div className={s2.hwTitle}>Homework #1</div>
             <div className={s2.hw}>
-                {/*проверка отображения (не менять)*/}
                 <div>
                     <Message message={message0} />
                     <FriendMessage message={friendMessage0} />
                 </div>
 
-                {/*для автоматической проверки дз (не менять)*/}
                 <MessageSender M={Message} />
             </div>
         </div>
